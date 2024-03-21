f286af725036f982440562497d96d9eb96c55ae2
psm817 <5004jse@gmail.com>
2024-03-21 14:15:43 +0900

    게시물 수정 기능 구현

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -88,6 +88,38 @@ public class Main {
                 System.out.printf("내용 : %s\n", foundArticle.body);
             }
 
+            // ===== 게시물 수정하기 =====
+            else if(cmd.startsWith("article modify ")) {
+                String[] cmdBits = cmd.split(" ");
+
+                int id = Integer.parseInt(cmdBits[2]);
+
+                Article foundArticle = null;
+
+                for(int i = 0; i < articles.size(); i++) {
+                    Article article = articles.get(i);
+
+                    if(article.id == id) {
+                        foundArticle = article;
+                        break;
+                    }
+                }
+
+                if(foundArticle == null) {
+                    System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
+                    continue;
+                }
+
+                System.out.printf("제목 : ");
+                String title = sc.nextLine();
+                System.out.printf("내용 : ");
+                String body = sc.nextLine();
+
+                foundArticle.title = title;
+                foundArticle.body = body;
+                System.out.printf("%d번 게시물이 수정되었습니다.\n", id);
+            }
+
             // ===== 게시물 삭제 =====
             else if(cmd.startsWith("article delete ")) {
                 String[] cmdBits = cmd.split(" ");
